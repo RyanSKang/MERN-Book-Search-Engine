10764cc253006f343ca364129b4db00af763fa94
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-08-06 16:05:19 -0600

    finished connecting server.js after complete schemas

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
 // Importing typeDefs and resolvers
-
+const { typeDefs, resolvers } = require('./schemas')
 
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -14,6 +14,15 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Creating an Apollo Server
+const server= new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// Use Apollo server with Express application
+server.applyMiddleware({app});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
