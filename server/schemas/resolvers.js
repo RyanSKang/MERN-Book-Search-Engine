09f88b925355983bccace760aa40062984812322
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-08-06 16:14:28 -0600

    trying to fix bug

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -47,7 +47,7 @@ const resolvers = {
             if (context.user) {
                 const savedBook = User.findByIdAndUpdate(
                     { _id: context.user._id },
-                    { $push: { savedBooks: newBook } },
+                    { $push: { savedBooks: {newBook} } },
                     { new: true }
                 );
                 return savedBook;
